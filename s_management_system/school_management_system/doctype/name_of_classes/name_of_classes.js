5e08375256f219fbc23eaf269e83f50d72c6bb63
delon.pyle <anthonydelon18@gmail.com>
2024-10-07 13:32:57 +0000

    Classes and Schedule Doctype created

---
+++ s_management_system/school_management_system/doctype/name_of_classes/name_of_classes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Delon Pyle and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Name Of Classes", {
+// 	refresh(frm) {
+
+// 	},
+// });
